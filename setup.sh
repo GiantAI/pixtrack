7530372e2db774338e5998ad456ed17fdc1fccb3
Prajwal Chidananda <prajwal.chidananda@giant.ai>
2022-04-05 01:21:34 +0000

    Add docker file, update instant-ngp installation

--- setup.sh
+++ setup.sh
@@ -9,12 +9,18 @@ pip3 install -e .
 # Install pixloc
 cd $ROOT/pixloc
 pip3 install -e .
-python3 -m pixloc.download --select checkpoints
+#python3 -m pixloc.download --select checkpoints
 
 # Install pixsfm
 cd $ROOT/pixel-perfect-sfm
-pip3 install -r requirements.txt
 pip3 install -e .
+
+# Install instant-ngp
+cd $ROOT/instant-ngp
+export PATH="/usr/local/cuda-11.1/bin:$PATH"
+export LD_LIBRARY_PATH="/usr/local/cuda-11.1/lib64:$LD_LIBRARY_PATH"
+cmake . -B build
+cmake --build build --config RelWithDebInfo -j 16
 
 cd $ROOT
 export PYTHONPATH=$PYTHONPATH:$ROOT
