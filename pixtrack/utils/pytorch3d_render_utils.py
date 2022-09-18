4f1a4cf9398767ea34edcf510a29e2dea3b711b1
Prajwal Chidananda <prajwal.chidanand@gmail.com>
2022-09-18 23:43:59 +0000

    Create sfm from obj files

---
+++ pixtrack/utils/pytorch3d_render_utils.py
@@ -0,0 +1,123 @@
+import torch
+import trimesh
+import numpy as np
+import pycolmap
+from scipy.spatial.transform import Rotation as R
+# Util function for loading meshes
+from pytorch3d.io import load_objs_as_meshes, load_obj
+
+# Data structures and functions for rendering
+from pytorch3d.structures import Meshes
+from pytorch3d.vis.plotly_vis import AxisArgs, plot_batch_individually, plot_scene
+from pytorch3d.vis.texture_vis import texturesuv_image_matplotlib
+from pytorch3d.renderer import (
+    look_at_view_transform,
+    look_at_rotation,
+    FoVPerspectiveCameras,
+    PerspectiveCameras,
+    PointLights, 
+    AmbientLights,
+    DirectionalLights, 
+    Materials, 
+    RasterizationSettings, 
+    MeshRenderer, 
+    MeshRasterizer,  
+    SoftPhongShader,
+    TexturesUV,
+    TexturesVertex
+)
+from hloc.utils.read_write_model import Camera, Image
+
+def create_look_at_camera_poses(radius, subdivisions=2):
+    sph = trimesh.creation.icosphere(subdivisions=subdivisions, 
+                                     radius=radius, 
+                                     color=None)
+    v = sph.vertices
+    Rs = [look_at_rotation(torch.tensor(v[i]).unsqueeze(0).float()).squeeze().numpy() for i in range(v.shape[0])]
+    Rs = [R.from_matrix(x).as_matrix() for x in Rs]
+    Ts = [-Rs[i].T @ v[i] for i in range(v.shape[0])]
+    Rs = torch.tensor(np.stack(Rs))
+    Ts = torch.tensor(np.stack(Ts))
+    return Rs, Ts
+
+def create_look_at_poses_for_mesh(mesh_path, subdivisions=2):
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0")
+        torch.cuda.set_device(device)
+    else:
+        device = torch.device("cpu")
+    mesh = load_objs_as_meshes([mesh_path], device=device)
+    torch.min(mesh.verts_list()[0], dim=0).values
+    mesh_min = torch.min(mesh.verts_list()[0], dim=0).values
+    mesh_max = torch.max(mesh.verts_list()[0], dim=0).values
+    max_dist = torch.sqrt(torch.sum((mesh_max - mesh_min) ** 2))
+    max_dist = float(max_dist)
+    Rs, Ts = create_look_at_camera_poses(max_dist, subdivisions)
+    return Rs, Ts, mesh
+
+def render_image(mesh, fx, fy, cx, cy, W, H, R, T, device='cuda:0'):
+    # Load mesh
+    #mesh = load_objs_as_meshes([mesh_path], device=device)
+
+    # Create cameras
+    assert fx == fy
+    focal_length = fx
+    principal_point = (cx, cy)
+    image_size = (W, H)
+    cameras = PerspectiveCameras(focal_length=focal_length,
+                             principal_point=(principal_point,),
+                             device=device, 
+                             R=R, 
+                             T=T,
+                             in_ndc=False,
+                             image_size=(image_size,))
+
+    # Rasterization Settings
+    raster_settings = RasterizationSettings(
+                             image_size=image_size, 
+                             blur_radius=0.0, 
+                             faces_per_pixel=1,)
+
+    # Create lights
+    lights = AmbientLights(device=device)
+
+    # Create renderer
+    renderer = MeshRenderer(
+                            rasterizer=MeshRasterizer(
+                                 cameras=cameras, 
+                                 raster_settings=raster_settings
+                            ),
+                            shader=SoftPhongShader(
+                                 device=device, 
+                                 cameras=cameras,
+                                 lights=lights
+                            )
+                            )
+
+    # Render image
+    image = renderer(mesh).squeeze().cpu().numpy()
+
+    return image
+
+def create_colmap_image_from_pytorch3d_RT(R, T, image_name, image_id, camera_id):
+    tr = np.eye(3)
+    tr[0, 0] = -1.
+    tr[1, 1] = -1.
+    rot = R @ tr
+    qvec = pycolmap.rotmat_to_qvec(rot.T)
+    tvec = T
+    image = Image(id=image_id, qvec=qvec, tvec=tvec,
+                  camera_id=camera_id, name=image_name,
+                  xys=[], point3D_ids=[])
+    return image
+
+def create_colmap_camera(w, h, f, cx, cy, k1, camera_id=1, model_id='SIMPLE_RADIAL'):
+    assert model_id == 'SIMPLE_RADIAL'
+    params = tuple([f, cx, cy, k1])
+    cam = Camera(id=camera_id,
+                 model=model_id,
+                 width=w,
+                 height=h,
+                 params = np.array(params))
+    return cam
+
---
+++ scripts/create_sfm_from_obj.py
@@ -0,0 +1,130 @@
+import os
+import sys
+import argparse
+from pathlib import Path
+import numpy as np
+import pycolmap
+from pixtrack.utils.pytorch3d_render_utils import (create_look_at_poses_for_mesh, 
+                                                   render_image,
+                                                   create_colmap_camera,
+                                                   create_colmap_image_from_pytorch3d_RT)
+from hloc import (extract_features, 
+                  match_features, 
+                  reconstruction, 
+                  pairs_from_exhaustive)
+import hloc.triangulation as triangulation
+from hloc.triangulation import (run_triangulation, 
+                                create_db_from_model,
+                                import_features, 
+                                import_matches, 
+                                geometric_verification)
+from hloc.reconstruction import (create_empty_db, 
+                                 import_images, 
+                                 get_image_ids)
+from hloc.utils.read_write_model import (write_images_binary, 
+                                         write_points3D_binary,
+                                         write_cameras_binary)
+import cv2
+from tqdm import tqdm
+
+if __name__ == '__main__':
+    object_name = os.environ['OBJECT']
+    #default_output_path = Path(os.environ['PIXSFM_OUTPUTS']) / object_name
+    default_output_path = Path(os.environ['PIXTRACK_OUTPUTS']) / 'nerf_sfm' / object_name
+    #default_dataset_path = Path(os.environ['PIXSFM_DATASETS']) / object_name
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mesh_path', required=True, type=Path)
+    parser.add_argument('--output_path', default=default_output_path, type=Path)
+    parser.add_argument('--dataset_path', default=default_output_path, type=Path)
+    args = parser.parse_args()
+
+    mesh_path = args.mesh_path
+    Rs, Ts, mesh = create_look_at_poses_for_mesh(mesh_path, subdivisions=2)
+    fx = 2700.
+    fy = 2700.
+    cx = 512. * 3
+    cy = 512. * 3
+    W = 1024 * 3
+    H = 1024 * 3
+
+    dataset_path = args.dataset_path
+    dataset_path.mkdir(parents=True, exist_ok=True)
+    mapping_path = dataset_path / 'mapping'
+    mapping_path.mkdir(parents=True, exist_ok=True)
+    colmap_images = {}
+    colmap_camera_id = 1
+    colmap_camera = create_colmap_camera(W, H, fx, cx, cy, k1=0.)
+    colmap_cameras = {colmap_camera_id: colmap_camera}
+    for i in tqdm(range(Rs.shape[0])):
+        R = Rs[i].unsqueeze(0)
+        T = Ts[i].unsqueeze(0)
+        image = render_image(mesh, fx, fy, cx, cy, W, H, R, T)
+        image = (image[..., :3] * 255.).astype(np.uint8)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        img_name = ('%d.png' % (i + 1)).zfill(8)
+        cv2.imwrite(str(mapping_path / img_name), image)
+
+        colmap_image_name = 'mapping/' + img_name
+        colmap_image_id = i + 1
+        colmap_image = create_colmap_image_from_pytorch3d_RT(R[0], T[0], 
+                                                          colmap_image_name,
+                                                          colmap_image_id,
+                                                          colmap_camera_id)
+        colmap_images[colmap_image_id] = colmap_image
+
+    # Extract features and matches
+    images = Path(dataset_path)
+    outputs = Path(args.output_path)
+    
+    sfm_pairs = outputs / 'pairs-sfm.txt'
+    loc_pairs = outputs / 'pairs-loc.txt'
+    features = outputs / 'features.h5'
+    matches = outputs / 'matches.h5'
+    raw_dir = outputs / "raw"
+    sfm_dir = outputs / "sfm"
+    
+    feature_conf = extract_features.confs['superpoint_max']
+    matcher_conf = match_features.confs['superglue']
+    matcher_conf['model']['weights'] = 'indoor'
+    
+    references = [str(p.relative_to(images)) for p in (images / 'mapping/').iterdir()]
+    print(len(references), "mapping images")
+    
+    extract_features.main(feature_conf, images, image_list=references, feature_path=features)
+    pairs_from_exhaustive.main(sfm_pairs, image_list=references)
+    match_features.main(matcher_conf, sfm_pairs, features=features, matches=matches)
+
+    pairs = sfm_pairs
+    assert features.exists(), features
+    assert pairs.exists(), pairs
+    assert matches.exists(), matches
+
+    
+    raw_dir.mkdir(parents=True, exist_ok=True)
+
+    database = raw_dir / 'database.db'
+    create_empty_db(database)
+    camera_mode = 'AUTO'
+    import_images(image_dir=images, 
+                  database_path=database, 
+                  camera_mode=camera_mode, 
+                  image_list=None)
+    image_ids = get_image_ids(database)
+    import_features(image_ids, database, features)
+    import_matches(image_ids, database, pairs, matches,
+                   min_match_score=None, 
+                   skip_geometric_verification=False)
+    geometric_verification(database, pairs, verbose=True)
+
+    write_images_binary(colmap_images, raw_dir / 'images.bin')
+    write_cameras_binary(colmap_cameras, raw_dir / 'cameras.bin')
+    write_points3D_binary({}, raw_dir / 'points3D.bin')
+
+    reference = pycolmap.Reconstruction(raw_dir)
+    sfm_dir.mkdir(parents=True, exist_ok=True)
+    database = sfm_dir / 'database.db'
+    image_ids2 = create_db_from_model(reference, database)
+    triangulation.main(sfm_dir, raw_dir, images, 
+                       pairs, features, matches,
+                       skip_geometric_verification=False, 
+                       min_match_score=None, verbose=True)
